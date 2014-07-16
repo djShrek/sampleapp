34b612141d171a11830e0849ec39cd88a2e848b1
djShrek <boho527@gmail.com>
2014-07-15 23:56:31 -0700

    finished tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,29 +14,40 @@ describe "Static pages" do
   # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 describe "Home page" do
- before { visit root_path }
- let(:heading) { 'Sample App' }
- let(:page_title) {''}
 
- it_should_behave_like "all static pages"
- it { should_not have_selector 'title', text: '| Home'}
+  before { visit root_path }
+  let(:heading) { 'Sample App' }
+  let(:page_title) {''}
 
- describe "for signed-in users" do
-  let(:user) { FactoryGirl.create(:user) }
-  before do
-    FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-    FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-    sign_in user
-    visit root_path
-  end
+  it_should_behave_like "all static pages"
+  it { should_not have_selector 'title', text: '| Home'}
 
-  it "should render the user's feed" do
-    user.feed.each do |item|
-      page.should have_selector("li##{item.id}", text: item.content)
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
+
+    describe "follower/following counts" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        other_user.follow!(user)
+        visit root_path
+      end
+
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 followers", href: followers_user_path(user)) }
     end
   end
- end
-
 end
 
 describe "Help page" do
